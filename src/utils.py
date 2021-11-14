ea1d5c52d4c3d1e4781604dcf655a90d7964a64b
Matheus Santiago <matheus.n.santiago99@gmail.com>
2021-11-14 16:51:20 -0300

    add função que extrai a data do nome do zip
    Isso é para resolver o problema da data em que houve um erro de
    digitação no XML a respeito da data. Eu consertei isso tirando a data do
    nome do zip (que é bem confiável do que o XML)

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,17 @@
 import json
+from pathlib import Path
+import re
+from datetime import datetime
+
 
 def get_env_variable(key):
     with open("credentials.json", "r") as f:
         cred = json.loads(f.read())
 
         return cred[key]
+
+def extract_date_from_zip_path(zip_path: Path) -> datetime.date:
+    match = re.search(r"\d{4}-\d{2}-\d{2}", zip_path)
+    date = datetime.strptime(match.group(), "%Y-%m-%d").date()
+        
+    return date

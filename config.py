7e05afb269765783f6591512d12efff91c569a54
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2022-01-06 09:20:43 -0300

    add suporte para o cosmos db

--- config.py
+++ config.py
@@ -12,3 +12,9 @@ mysql = {
     "PASSWORD": "oasuet10",
     "DATABASE": "dou_db",
 }
+
+cosmos = {
+    "ACCOUNT_URI": "https://sumula-dou-db.documents.azure.com:443/",
+    "ACCOUNT_KEY": "4sIVaLCH7R1eDdPleSZ5CvVEhoyqUs8NVtiDZR5CAwfYCjQWjjKPJiNMYdlsQvb5eNWdccjIbC0sCTplysEeqw==",
+    "DATABASE_ID": "sumula-dou-db",
+}
